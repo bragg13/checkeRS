4843442e8ab9f9c1562f71c5be9e889ab221cd30
Andrea Bragante <andrea@MacBook-Pro-di-Andrea.local>
2026-02-04 15:23:03 +0100

    server scaffolding done?

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,6 @@
-use renet::{
-    ConnectionConfig, NETCODE_USER_DATA_BYTES, RenetConnectionConfig, RenetServer,
-    ServerAuthentication, ServerConfig, ServerEvent,
-};
+use cli_log::{info, trace};
+use color_eyre::owo_colors::colors::Default;
+use renet::{ConnectionConfig, DefaultChannel, RenetServer, ServerEvent};
 use renet_netcode::{
     ClientAuthentication, NETCODE_USER_DATA_BYTES, NetcodeClientTransport, NetcodeServerTransport,
     ServerAuthentication, ServerConfig,
@@ -10,6 +9,9 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::time::{Duration, Instant, SystemTime};
 
 fn main() {
+    env_logger::Builder::from_default_env()
+        .filter_level(cli_log::LevelFilter::Trace) // Show all logs
+        .init();
     let mut server = RenetServer::new(ConnectionConfig::default());
 
     // Setup transport layer using renet_netcode
@@ -26,6 +28,7 @@ fn main() {
     };
     let mut transport = NetcodeServerTransport::new(server_config, socket).unwrap();
     let mut last_updated = Instant::now();
+    trace!("🕹 server listening on {}", SERVER_ADDR);
 
     loop {
         let now = Instant::now();
@@ -34,11 +37,29 @@ fn main() {
         server.update(duration);
         transport.update(duration, &mut server).unwrap();
 
+        // handles events
         while let Some(event) = server.get_event() {
             match event {
-                ServerEvent::ClientConnected { client_id } => todo!(),
-                ServerEvent::ClientDisconnected { client_id, reason } => todo!(),
+                ServerEvent::ClientConnected { client_id } => {
+                    info!("Client connected! {}", client_id);
+                }
+                ServerEvent::ClientDisconnected { client_id, reason } => {
+                    info!(":( Client disconnected! {}", client_id);
+                }
             }
         }
+
+        // receive messges
+        for client_id in server.clients_id() {
+            while let Some(msg) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
+            {
+                info!("Received message from {} saying {:?}.", client_id, msg);
+
+                // server.send_message or handling...
+            }
+        }
+
+        transport.send_packets(&mut server);
+        std::thread::sleep(Duration::from_millis(50));
     }
 }
