3d7e01874ac94c8bc876a9f351852a1ec7872041
Andrea Bragante <andrea@MacBook-Pro-di-Andrea.local>
2026-02-09 20:14:13 +0100

    main menu UI finished

--- client/src/main_menu.rs
+++ client/src/main_menu.rs
@@ -1,10 +1,11 @@
-use crossterm::event::{KeyCode, KeyEvent};
+use cli_log::info;
+use crossterm::event::{Event, KeyCode, KeyEvent};
 use ratatui::{
     layout::{Constraint, Layout, Margin, Rect},
     style::{Color, Style},
     widgets::{Block, Borders, Paragraph, Widget},
 };
-use tui_input::Input;
+use tui_input::{Input, backend::crossterm::EventHandler};
 
 use crate::{Scene, SceneTransition, game::GameScene};
 #[derive(Debug)]
@@ -31,7 +32,7 @@ impl Widget for &MainMenuScene {
         ])
         .split(inner);
 
-        let username = Block::default()
+        let username_block = Block::default()
             .title("Username")
             .borders(Borders::ALL)
             .border_style(if self.focused == 0 {
@@ -40,9 +41,10 @@ impl Widget for &MainMenuScene {
                 Style::default()
             });
 
-        username.render(chunks[0], buf);
+        Paragraph::new(self.username_in.value()).render(username_block.inner(chunks[0]), buf);
+        username_block.render(chunks[0], buf);
 
-        let addr = Block::default()
+        let addr_block = Block::default()
             .title("Address")
             .borders(Borders::ALL)
             .border_style(if self.focused == 1 {
@@ -50,14 +52,15 @@ impl Widget for &MainMenuScene {
             } else {
                 Style::default()
             });
-        addr.render(chunks[1], buf);
+        Paragraph::new(self.addr_in.value()).render(addr_block.inner(chunks[1]), buf);
+        addr_block.render(chunks[1], buf);
 
         let button = Paragraph::new("[ Connect ]")
             .centered()
             .block(Block::default().borders(Borders::ALL))
             .style(if self.focused == 2 {
                 Style::default().bg(Color::Yellow)
-            } else if self.submit {
+            } else if self.can_submit() {
                 Style::default().fg(Color::White)
             } else {
                 Style::default().fg(Color::DarkGray)
@@ -76,11 +79,19 @@ impl MainMenuScene {
             focused: 0,
         }
     }
+    fn can_submit(&self) -> bool {
+        !self.username_in.value().is_empty() && !self.addr_in.value().is_empty()
+    }
+
     pub fn handle_input(&mut self, key_event: KeyEvent) -> SceneTransition {
         match key_event.code {
             KeyCode::Enter => {
-                self.submit = true;
-                SceneTransition::ToGame
+                if self.can_submit() && self.focused == 2 {
+                    self.submit = true;
+                    SceneTransition::ToGame
+                } else {
+                    SceneTransition::None
+                }
             }
             KeyCode::Down => {
                 if self.focused < 2 {
@@ -94,7 +105,17 @@ impl MainMenuScene {
                 }
                 SceneTransition::None
             }
-            _ => SceneTransition::None,
+            _ => match self.focused {
+                0 => {
+                    self.username_in.handle_event(&Event::Key(key_event));
+                    SceneTransition::None
+                }
+                1 => {
+                    self.addr_in.handle_event(&Event::Key(key_event));
+                    SceneTransition::None
+                }
+                _ => SceneTransition::None,
+            },
         }
     }
 }
